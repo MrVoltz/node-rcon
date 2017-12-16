e8dcebe27f8974f32484a3f50b16dee9e6dc5cfa
MrVoltz <uzivatelmp@gmail.com>
2017-12-16 18:36:34 +0100

    Fixed formatting, added events (auth, close)

--- node-rcon.js
+++ node-rcon.js
@@ -1,144 +1,184 @@
-var net = require('net');
+var net = require('net'),
+  events = require('events'),
+  util = require('util');
 
-var AUTH_TYPE=0x03;
-var COMMAND_TYPE=0x02;
-var AUTH_RES_TYPE=0x02;
-var TIMEOUT=2000; //time to wait for a response
+var AUTH_TYPE = 0x03;
+var COMMAND_TYPE = 0x02;
+var AUTH_RES_TYPE = 0x02;
+var TIMEOUT = 10000; //time to wait for a response
 
-module.exports.newHandle = function(){
-this.socket=null;
-this.reqID=1;
-this.authed = false;
-this.online = false;
+var Rcon = function() {
+  this.socket = null;
+  this.reqID = 1;
+  this.authed = false;
+  this.online = false;
 
-this.isOnline = function() {return this.online;}//return online status of connection
+  //queue management
+  this.queue = {}; //keeps track of commands sent
+};
 
-//queue management
-this.queue = {}; //keeps track of commands sent
-/**
-* Add a call to the queue and start the timeout.
-* @param {Number} Request id
-* @param {Function} callback
-*/
-this._addQueue = function(id, callback){
-	if (typeof(callback)==='undefined')return;
-	var q = this.queue;
-	var t = setTimeout(function(){
-		delete q[id]; //drop the timed out request from the queue 
-		callback({error:'Timed out'});
-	},TIMEOUT);
-	this.queue[id]={callback:callback, timeout:t};
+util.inherits(Rcon, events.EventEmitter);
+
+Rcon.prototype.isOnline = function() {
+  return this.online;
+};
+
+Rcon.prototype.isAuthed = function() {
+  return this.online && this.authed;
 };
 
 /**
-* Check if we have a request matching the response
-* @param {Object} response object
-*/
-this._processQueue = function(response){
+ * Add a call to the queue and start the timeout.
+ * @param {Number} Request id
+ * @param {Function} callback
+ */
+Rcon.prototype._addQueue = function(id, callback) {
+  if (typeof callback === 'undefined') return;
+  var q = this.queue;
+  var t = setTimeout(function() {
+    delete q[id]; //drop the timed out request from the queue
+    callback({
+      error: 'Timed out'
+    });
+  }, TIMEOUT);
+
+  this.queue[id] = {
+    callback: callback,
+    timeout: t
+  };
+};
+
+/**
+ * Check if we have a request matching the response
+ * @param {Object} response object
+ */
+Rcon.prototype._processQueue = function(response) {
   //do we have anything on the queue that matches.
-  if(response.reqID in this.queue){
+  if (response.reqID in this.queue) {
     var q = this.queue[response.reqID];
     //clear the timeout!
     clearTimeout(q.timeout);
     //callback
     q.callback(null, response);
     //delete from the queue
-    delete this.queue[response.reqID];	
+    delete this.queue[response.reqID];
   }
-}
-
+};
 
 //connect command
-this.connect = function(ip,port,password,callback){
-	if(typeof(callback)==='undefined'){callback = function(err, response){};}
-        this.socket = net.createConnection(port,ip);
-        this.socket.rcon = this;
-        this.socket.on('error',function(err){return callback(err);});
-        this.socket.on('close',function(){this.rcon.authed=false;this.rcon.online=false});
-        this.socket.on('connect',function(){
-          this.rcon.online=true;
-          this.write(this.rcon.makePacket(0,AUTH_TYPE,password));
-        });	
-        this.socket.on('data',function(data){
-          var responses = this.rcon.readPacket(data);
-	  for(var i=0; i<responses.length;i++){
-	    var response = responses[i]; 
-            if(response.type === AUTH_RES_TYPE){
-              if(response.reqID == -1){
-                this.destroy();
-                callback('AUTH FAILED');
-                return;	
-              }else{
-		this.rcon.authed=true;
-		callback(null, response); 
-		return;
-	      }
-            }
-	    this.rcon._processQueue(response);
-	  }
-        });
-      };
+Rcon.prototype.connect = function(ip, port, password, callback) {
+  if (typeof(callback) === 'undefined') {
+    callback = function(err, response) {};
+  }
+
+  this.ip = ip;
+  this.socket = net.createConnection(port, ip);
+  this.socket.rcon = this;
+  this.socket.on('error', function(err) {
+    return callback(err);
+  });
+  this.socket.on('close', function() {
+    this.rcon.authed = false;
+    this.rcon.online = false;
+
+    this.rcon.emit("close");
+  });
+  this.socket.on('connect', function() {
+    this.rcon.online = true;
+    this.write(this.rcon.makePacket(0, AUTH_TYPE, password));
+  });
+  this.socket.on('data', function(data) {
+    var responses = this.rcon.readPacket(data);
+    for (var i = 0; i < responses.length; i++) {
+      var response = responses[i];
+      if (response.type === AUTH_RES_TYPE) {
+        if (response.reqID == -1) {
+          this.destroy();
+          callback('AUTH FAILED');
+          return;
+        } else {
+          this.rcon.authed = true;
+          this.rcon.emit("auth", this.rcon.ip);
+          callback(null, response);
+          return;
+        }
+      }
+      this.rcon._processQueue(response);
+    }
+  });
+};
 
 //end connection
-this.end = function(){
-	this.socket.end();
-}
+Rcon.prototype.end = function() {
+  this.socket.end();
+};
 
+Rcon.prototype.sendCommand = function(command, callback) {
+  var msg;
+  if (typeof(callback) === 'undefined') {
+    callback = function(err, response) {};
+  }
+  if (!this.online) {
+    msg = {
+      "error": 'Not Online'
+    };
+    callback(msg);
+    return msg;
+  }
+  if (!this.authed) {
+    msg = {
+      "error": 'Not Authenticated'
+    };
+    callback(msg);
+    return msg;
+  }
 
-this.sendCommand = function(command,callback) {
-	var msg;
-	if (typeof(callback)==='undefined') {callback = function(err, response){};}
-        if(!this.online){msg={"error":'Not Online'};callback(msg);return msg;}
-        if(!this.authed){msg={"error":'Not Authenticated'};callback(msg); return msg;}
-        var id = this.reqID+0;
-	this.socket.write(this.makePacket(id,COMMAND_TYPE,command));
-        this._addQueue(id, callback);
-	
-	this.reqID +=1;
-        return {"reqID":id};
-       };
+  var id = this.reqID + 0;
+  this.socket.write(this.makePacket(id, COMMAND_TYPE, command));
+  this._addQueue(id, callback);
 
+  this.reqID += 1;
+  return {
+    "reqID": id
+  };
+};
 
-       
 //makes a packet for RCON
-this.makePacket = function(req,type,S1){
-        var b = new Buffer(14+S1.length);
-        b.writeInt32LE(10+S1.length,0);
-        b.writeInt32LE(req,4);
-        b.writeInt32LE(type,8);
-        b.write(S1,12);
-        b.writeInt16LE(0,(12+S1.length));
-        return b;      
-      }
+Rcon.prototype.makePacket = function(req, type, S1) {
+  var b = new Buffer(14 + Buffer.byteLength(S1));
+  b.writeInt32LE(10 + Buffer.byteLength(S1), 0);
+  b.writeInt32LE(req, 4);
+  b.writeInt32LE(type, 8);
+  b.write(S1, 12);
+  b.writeInt16LE(0, (12 + Buffer.byteLength(S1)));
+  return b;
+};
 
 //read a packet out into a JSON object
-this.readPacket = function(packet){
-  var responses=[];
-  var haveMore=true;
+Rcon.prototype.readPacket = function(packet) {
+  var responses = [];
+  var haveMore = true;
   var size, res;
-  while(haveMore){
+  while (haveMore) {
     size = packet.readInt32LE(0);
     res = {
       size: size,
       reqID: packet.readInt32LE(4),
       type: packet.readInt32LE(8),
-      data:packet.toString('utf8',12,size+2) //don't read beyond response size
-      };
+      data: packet.toString('utf8', 12, size + 2) //don't read beyond response size
+    };
     responses.push(res);
     //there are more to read, at least one additional response
-    if(packet.length > size+4){
-	//position on the next one
-	packet = packet.slice(size+4);
-    }
-    else{
+    if (packet.length > size + 4) {
+      //position on the next one
+      packet = packet.slice(size + 4);
+    } else {
       //no more responses
-      haveMore=false;
+      haveMore = false;
     }
   }
   //always return an array even if only one response
   return responses;
-  };
-return this;
 };
 
-
+module.exports = Rcon;
